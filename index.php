8de434e9abea6eb0eff6c779ce7ac3acfd10e9fa
Julian Purser <juliangd@hotmail.co.uk>
2021-08-08 21:20:54 +0100

    Linked stylesheet in index.php

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landtech Articles</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
 <?php 
     include 'includes/article-output.php';  
     include 'includes/author-table.php';    
-    
+
     // Conversion Function
     function conversionRate($sales, $views) {
         $conversionRate = ($sales/$views) * 100;
